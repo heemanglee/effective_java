72b7f08be8ca291f1e5de8043b7e37eb6ee62e90
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-01 18:07:28 +0900

    first commit

---
+++ src/test/java/example/effective_java/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.effective_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
